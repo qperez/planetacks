b554c482f73e72fff178497e273005afe8f4cf17
qperez <archerdu70@live.fr>
2015-11-27 16:47:57 +0100

    Création de la classe Astre et de la documentation

---
+++ src/Astre.java
@@ -0,0 +1,19 @@
+/**
+ * <b>Classe permettant de mod&eacute;liser un astre.</b>
+ * Un Astre est caract&eacute;ris&eacute; par les informations suivantes :
+ * <ul>
+ * <li>Un nom.</li>
+ * <li>Une position X.</li>
+ * <li>Une position Y.</li>
+ * <li>Un demi-grand axe.</li>
+ * <li>Un demi-petit axe.</li>
+ * </ul>
+ */
+public class Astre {
+    //JavaDoc à venir
+    private String nom;
+    private int positionX;
+    private int positionY;
+    private int demiGrandAxe;
+    private int demiPetitAxe;
+}
