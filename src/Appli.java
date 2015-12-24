dc01cbfb879eab14201cad7738c64fa70de3d605
qperez <archerdu70@live.fr>
2015-12-24 10:06:39 +0100

    Création du modèle MVC, début de l'implémentation de l'interface graphique. Fin de la modélisation des classes Astre, Étoile et Satellite

--- src/Appli.java
+++ src/Appli.java
@@ -5,11 +5,12 @@ import java.io.IOException;
  */
 public class Appli {
     public static void main(String[] args) {
-        /*javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-
+                Modele modele = new Modele();
+                Fenetre fenetre = new Fenetre(modele);
             }
-        });*/
+        });
         System.out.println("Hello world, it's was the first commit in the space beacause ponies are the most beautiful");
     }
 }
